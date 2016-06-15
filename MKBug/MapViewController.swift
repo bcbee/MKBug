d8f20bad63f85ff6bf3c48b9ae42da8aabdcd3f0
Brendan Boyle <brendancboyle@me.com>
2016-06-15 16:09:37 -0700

    Added bug project

---
+++ MKBug/MapViewController.swift
@@ -0,0 +1,43 @@
+//
+//  MapViewController.swift
+//  MKBug
+//
+//  Created by Brendan Boyle on 6/15/16.
+//  Copyright © 2016 DBZ Technology. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+
+    @IBOutlet weak var myMap: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        myMap.removeFromSuperview()
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
